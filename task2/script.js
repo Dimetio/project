8f48f3fc71742fdb31657fa3ece3422f62b36963
Dimetrio <dubinin.dmitry@mail.ru>
2019-12-19 13:20:52 +0300

    completed task 2

---
+++ task2/script.js
@@ -0,0 +1,65 @@
+let money = +prompt("Ваш бюджет на месяц?", '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+const appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+// for (let i = 0; i < 2; i++) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+//   let b = prompt("Во сколько обойдется?", '');
+//   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//     console.log('done');
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log('нужно ввести значение');
+//     i--;
+//   }
+// }
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+//   let b = prompt("Во сколько обойдется?", '');
+//   if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//     console.log('done');    
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log('нужно ввести значение');
+//     i--;  
+//   }
+//  i++;
+// }
+
+// let i = 0;
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+//   let b = prompt("Во сколько обойдется?", '');
+//   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//     console.log('done');
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log('нужно ввести значение');
+//     i--;
+//   }
+//   i++;
+// }
+// while (i < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+console.log('бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log('минималка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('среднячек');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('марожик');
+} else {
+  console.log('ошибка');
+}
