29ddb75ac973e8ac79b2e866116cea962721271c
ddubinin <dubinin.dmitry@mail.ru>
2020-09-08 14:38:21 +0300

    taks 3

--- task3/script.js
+++ task3/script.js
@@ -1,85 +1,67 @@
-let money;
-let time;
+let numberOfFilms;
 
 function start() {
-  money = +prompt("Ваш бюджет на месяц?", '');
-  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    numberOfFilms = +prompt('Сколько фильмов?', '');
 
-  while (isNaN(money) || money == "" || money == null) {
-    money = +prompt("Ваш бюджет на месяц?", '');
-  }
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов?', '');
+    }
 }
+
+
 start();
 
-const appData = {
-  budget: money,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: true
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-// обязательный расходы
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
-    let b = prompt("Во сколько обойдется?", '');
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-      console.log('done');
-      appData.expenses[a] = b;
-    } else {
-      console.log('нужно ввести значение');
-      i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних фильмов', ''),
+            b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieBD.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
-  }
 }
-chooseExpenses();
 
-// необязательные расходы
-function chooseOptExpenses() {
-  for (let i = 1; i <= 3; i++) {
-    let a = prompt("Введите необязательную статью расходов в этом месяце", '');
-    let b = prompt("Во сколько обойдется?", '');
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-      console.log('done');
-      appData.optionalExpenses[i] = b;
+//rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieBD.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieBD.count >= 10 && personalMovieBD < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieBD.count >= 30) {
+        console.log('Вы киноман');
     } else {
-      console.log('нужно ввести значение');
-      i--;
+        console.log('Произошла ошибка');
     }
-  }
 }
-chooseOptExpenses();
 
-// бюджет на день
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.budget / 30).toFixed();
-  console.log('бюджет: ' + appData.moneyPerDay);
-}
-detectDayBudget();
+//detectPersonalLevel();
 
-// уровень достатка
-function detectLevel() {
-  if (appData.moneyPerDay < 100) {
-    console.log('минималка');
-  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('среднячек');
-  } else if (appData.moneyPerDay > 2000) {
-    console.log('марожик');
-  } else {
-    console.log('ошибка');
-  }
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieBD);
+    }
 }
-detectLevel();
 
-// накопления
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt('сумма накоплений');
-    let percent = +prompt('под какой %?');
-    appData.monthIncome = save / 100 / 12 * percent;
-    console.log(`доход с депозита: ${appData.monthIncome}`);
-  }
+showMyDB(personalMovieBD.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieBD.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
 }
-checkSavings();
+
+// writeYourGenres();
