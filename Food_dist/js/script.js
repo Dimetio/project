cc9ea4dcbc14c6c146c5d4bf89263d4ef2dc72d0
ddubinin <dubinin.dmitry@mail.ru>
2020-10-22 20:56:37 +0300

    test axios

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -192,12 +192,23 @@ tabsParent.addEventListener('click', function(event) {
     return await res.json();
   };
 
-  getResource('http://localhost:3000/menu')
-    .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //   getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    // ******** axios **************
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
         });
-    });
+
+    // ********** end ***************
 
     // *********** Рендер без шаблона **************
     // getResource('http://localhost:3000/menu')
